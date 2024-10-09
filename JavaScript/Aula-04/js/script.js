fb480db60dba41207aff9340f7de830f627a931c
Bruno <132005298+BrunoRoheweder@users.noreply.github.com>
2024-10-09 18:57:38 -0400

    ajustando o script

--- JavaScript/Aula-04/js/script.js
+++ JavaScript/Aula-04/js/script.js
@@ -4,24 +4,20 @@ var body = document.getElementById("body")
 btn.innerHTML = "Abrir Texto"
 
 function Texto() {
-    // btn.addEventListener("click", function() {
+    btn.addEventListener("click", function() {
         body.appendChild(btn)
         div.innerHTML = "<p>Lorem Ipsum</p>"
         btn.innerHTML = "Fechar Texto"
-        setTimeout(Remover, 7000)
+        Remover()
         function Remover() {
-            // btn.addEventListener("click", function (){
+            btn.addEventListener("click", function (){
                 body.appendChild(btn)
                 div.innerHTML = " "
                 btn.innerHTML = "Abrir Texto"
                 Texto()
-            // })
+            })
         }
-    // })
-}
-
-for (var i = 0; i < 10; i++){
-    setTimeout(() => Texto(i), i * 8000);
+    })
 }
 
 Texto()
