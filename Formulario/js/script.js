0288a911612dc72248ffa1fe42de494159be6974
Bruno <132005298+BrunoRoheweder@users.noreply.github.com>
2024-10-14 20:26:28 -0400

    Fazendo att do prof

---
+++ Formulario/js/script.js
@@ -0,0 +1,12 @@
+// puxando/pegando as informações do input
+
+const form = document.getElementById("form")
+const usuario = document.getElementById("usuario")
+const email = document.getElementById("email")
+const senha = document.getElementById("senha")
+const senha2 = document.getElementById("senha2")
+
+
+form.addEventListener("submit", function(){
+    
+})
